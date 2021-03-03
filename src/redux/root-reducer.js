dfb4308da4eedb510c772b6e0e374d957f16e43c
Odira Eze <odira9026@gmail.com>
2021-03-03 21:39:03 +0100

    added redux-persist, setup configurations to store app data in localStorage using redux-persist and wrapping our app in redux persistGate

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,9 +1,19 @@
 import { combineReducers } from 'redux';
+import { persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage'; // this imports localStorage
 
 import userReducer from './user/user.reducer';
 import cartReducer from './cart/cart.reducer'; 
 
-export default combineReducers({
+const persistConfig = {
+    key: 'root',
+    storage,
+    whitelist: ['cart'] //an array containing the string names of any of d reducer we want to store
+}
+
+const rootReducer = combineReducers({
     user: userReducer,
     cart: cartReducer
 });
+
+export default persistReducer(persistConfig, rootReducer);
